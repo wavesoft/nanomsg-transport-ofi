6d8509f11091eb0e0a592efb6e3208bc3aa3f0b6
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-12-08 11:59:08 +0100

    Adding missing ofi.h header

--- patch-nanomsg.sh
+++ patch-nanomsg.sh
@@ -11,25 +11,27 @@ popd > /dev/null
 [ ! -f "${NANOMSG_DIR}/libnanomsg.pc.in" ] && echo -e "** FAILED **\nThis does not look like a nanomsg directory!" && exit 1
 
 # Check if we already have the ofi directory
-NANOMSG_TRANSPORT_OFI="${NANOMSG_DIR}/src/transports/ofi"
-[ -L "${NANOMSG_TRANSPORT_OFI}" ] && echo -e "** FAILED **\nOFI Transport seems to be installed already!" && exit 1
-[ -d "${NANOMSG_TRANSPORT_OFI}" ] && echo -e "** FAILED **\nOFI Transport seems to be installed by another source!" && exit 1
+NANOMSG_SRC_DIR="${NANOMSG_DIR}/src/transports"
+NANOMSG_TRANSPORTS_OFI_DIR="${NANOMSG_SRC_DIR}/transports/ofi"
+[ -L "${NANOMSG_TRANSPORTS_OFI_DIR}" ] && echo -e "** FAILED **\nOFI Transport seems to be installed already!" && exit 1
+[ -d "${NANOMSG_TRANSPORTS_OFI_DIR}" ] && echo -e "** FAILED **\nOFI Transport seems to be installed by another source!" && exit 1
 
 # Apply patch to global source
 echo ${NANOMSG_DIR}
-echo -n "Patch: "
+echo "Patching..."
 patch -p0 -d ${NANOMSG_DIR} < ${CURR_PATH}/patch/nanomsg-patch-global.patch
 [ $? -ne 0 ] && echo -e "** FAILED **\nUnable to apply patch!" && exit 1
 
 # Apply patch to makefile
-echo -n "Patch: "
 patch -d ${NANOMSG_DIR} < ${CURR_PATH}/patch/nanomsg-patch-makefile.patch
 [ $? -ne 0 ] && echo -e "** FAILED **\nUnable to apply patch!" && exit 1
 
 # Link sources directory
-echo -n "Link: "
-ln -v -s ${CURR_PATH}/src/transports/ofi ${NANOMSG_TRANSPORT_OFI}
-[ $? -ne 0 ] && echo -e "** FAILED **\nUnable to create symbolic link!" && exit 1
+echo "Copying..."
+cp -v -r ${CURR_PATH}/src/transports/ofi ${NANOMSG_TRANSPORTS_OFI_DIR}
+[ $? -ne 0 ] && echo -e "** FAILED **\nUnable to copy sources!" && exit 1
+cp -v -r ${CURR_PATH}/src/ofi.h ${NANOMSG_SRC_DIR}/ofi.h
+[ $? -ne 0 ] && echo -e "** FAILED **\nUnable to copy sources!" && exit 1
 
 # We are done
 echo "** SUCCESS **"

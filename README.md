3b607afb20faf572c25026074027372afc06010a
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-12-08 11:46:56 +0100

    Adding patch and updating README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # nanomsg-transport-ofi
-nanomsg transport for interfacing with the OpenFabrics Interfaces (OFI) Library (libfabric)
+
+Nanomsg transport for interfacing with the OpenFabrics Interfaces (OFI) Library (libfabric).
+
+__This project is under development and will not compile!__
+
+## How to compile
+
+ 1. Make sure you have [libfabric](http://ofiwg.github.io/libfabric/) installed in your system.
+ 2. Download the [latest sources of nanonsg](https://github.com/nanomsg/nanomsg/releases):
+```sh
+wget https://github.com/nanomsg/nanomsg/releases/download/0.8-beta/nanomsg-0.8-beta.tar.gz
+tar -zxf nanomsg-0.8-beta.tar.gz
+NANOMSG_SROUCES=$(pwd)/nanomsg-0.8-beta
+``` 
+ 3. Check-out `nanomsg-transport-ofi`:
+```sh
+git clone https://github.com/wavesoft/nanomsg-transport-ofi.git
+```
+ 4. Patch the sources of nanomsg, using the `patch-nanomsg.sh` script that comes with this project.
+```sh
+./patch-nanomsg.sh $NANOMSG_SROUCES
+```
+ 5. You can now compile nanomsg:
+```
+cd $NANOMSG_SROUCES
+./configure && make
+sudo make install
+```
---
+++ patch-nanomsg.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Validate input
+[ -z "$1" ] && echo "ERROR: Please specify the full path to the nanomsg sources!" && exit 1
+NANOMSG_DIR=$1
+pushd `dirname $0` > /dev/null
+CURR_PATH=`pwd`
+popd > /dev/null
+
+# Do some quick-check to make sure that's a nanomsg directory
+[ ! -f "${NANOMSG_DIR}/libnanomsg.pc.in" ] && echo -e "** FAILED **\nThis does not look like a nanomsg directory!" && exit 1
+
+# Check if we already have the ofi directory
+NANOMSG_TRANSPORT_OFI="${NANOMSG_DIR}/src/transports/ofi"
+[ -L "${NANOMSG_TRANSPORT_OFI}" ] && echo -e "** FAILED **\nOFI Transport seems to be installed already!" && exit 1
+[ -d "${NANOMSG_TRANSPORT_OFI}" ] && echo -e "** FAILED **\nOFI Transport seems to be installed by another source!" && exit 1
+
+# Apply patch to global source
+echo ${NANOMSG_DIR}
+echo -n "Patch: "
+patch -p0 -d ${NANOMSG_DIR} < ${CURR_PATH}/patch/nanomsg-patch-global.patch
+[ $? -ne 0 ] && echo -e "** FAILED **\nUnable to apply patch!" && exit 1
+
+# Apply patch to makefile
+echo -n "Patch: "
+patch -d ${NANOMSG_DIR} < ${CURR_PATH}/patch/nanomsg-patch-makefile.patch
+[ $? -ne 0 ] && echo -e "** FAILED **\nUnable to apply patch!" && exit 1
+
+# Link sources directory
+echo -n "Link: "
+ln -v -s ${CURR_PATH}/src/transports/ofi ${NANOMSG_TRANSPORT_OFI}
+[ $? -ne 0 ] && echo -e "** FAILED **\nUnable to create symbolic link!" && exit 1
+
+# We are done
+echo "** SUCCESS **"

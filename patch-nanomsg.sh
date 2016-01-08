edff70ef11369296c5e9e19386a8075cd0b67fd5
Ioannis Charalampidis <ioannischaralampidis@pb-d-128-141-47-114.cern.ch>
2016-01-08 11:54:58 +0100

    Making patch script smarter

--- patch-nanomsg.sh
+++ patch-nanomsg.sh
@@ -10,6 +10,33 @@ popd > /dev/null
 # Do some quick-check to make sure that's a nanomsg directory
 [ ! -f "${NANOMSG_DIR}/libnanomsg.pc.in" ] && echo -e "** FAILED **\nThis does not look like a nanomsg directory!" && exit 1
 
+# Get current git version
+GIT_VERSION=$(git rev-parse HEAD)
+
+# Check if we already have patched that source
+if [ -f "${NANOMSG_DIR}/.nanomsg-ofi-patch.version" ]; then
+
+	# Check if version matches
+	DIR_VERSION=$(cat "${NANOMSG_DIR}/.nanomsg-ofi-patch.version")
+	if [ "$DIR_VERSION" == "$GIT_VERSION" ]; then
+		echo "** UP TO DATE **"
+		exit 0
+	fi
+
+	# Just update sources
+	echo "Updating..."
+	cp -v -r ${CURR_PATH}/src/transports/ofi ${NANOMSG_TRANSPORTS_OFI_DIR}
+	[ $? -ne 0 ] && echo -e "** FAILED **\nUnable to copy sources!" && exit 1
+	cp -v -r ${CURR_PATH}/src/ofi.h ${NANOMSG_SRC_DIR}/ofi.h
+	[ $? -ne 0 ] && echo -e "** FAILED **\nUnable to copy sources!" && exit 1
+
+	# Update patch version
+	echo $GIT_VERSION > "${NANOMSG_DIR}/.nanomsg-ofi-patch.version"
+	echo "** UPDATED **"
+	exit 0
+
+fi
+
 # Check if we already have the ofi directory
 NANOMSG_SRC_DIR="${NANOMSG_DIR}/src"
 NANOMSG_TRANSPORTS_OFI_DIR="${NANOMSG_SRC_DIR}/transports/ofi"
@@ -39,4 +66,5 @@ echo "Re-creating autoconfig files (to apply patches)..."
 [ $? -ne 0 ] && echo -e "** FAILED **\nUnable to run aclocal/autoconf/automake!" && exit 1
 
 # We are done
+echo $GIT_VERSION > "${NANOMSG_DIR}/.nanomsg-ofi-patch.version"
 echo "** SUCCESS **"

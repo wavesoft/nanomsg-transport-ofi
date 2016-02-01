2b3e57c47bc31eb2806d7e60363b250f722c0fb3
Ioannis Charalampidis <icharala@macaliceichar.cern.ch>
2016-02-01 15:51:29 +0100

    Getting tests up-to-date

--- test/nn_standalone_func.h
+++ test/nn_standalone_func.h
@@ -4,7 +4,9 @@
 
 /* Define some functions that are provided by nanomsg */
 #define nn_alloc(sz,nop)	malloc(sz)
-#define nn_free(p)		free(p)
+#define nn_free(p)			free(p)
 #define alloc_assert(expr)	assert( expr != NULL )
+#define nn_fast(x) 			(x)
+#define nn_slow(x) 			(x)
 
 #endif /* NN_TEST_SHARED */
